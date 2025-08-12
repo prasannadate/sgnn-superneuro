1a7c2619846f37fef42c84c35c06120fccb3c739
Date <pgu@taipan.ornl.gov>
2025-08-12 11:52:42 -0400

    testing push

--- gnn_citation_networks_efficient.py
+++ gnn_citation_networks_efficient.py
@@ -6,21 +6,43 @@ from superneuromat import SNN
 DATASET_CONFIG = "configs/microseer/default_microseer_config.yaml"
 
 
-def load_paper_neurons(config, snn):
-	""" Loads the paper info and creates paper neurons in the snn
-
-		Args:
-			config: Configuration of the dataset from the yaml file
-			snn: SuperNeuroMAT SNN
-
-		Returns:
-			paper_to_neuron: Dictionary mapping paper IDs to neuron IDs
-			neuron_to_paper: Dictionary mapping neuron IDs to paper IDs
+class SGNN:
+	""" Manage citation networks data, create spiking graph neural networks (SGNNs)
 
 	"""
 
-	if config["dataset"] == "microseer":
-		with open("data/microseer/microseer.content")
+	def __init__(self):
+		""" Initialize all class variables
+
+		"""
+
+		# Count variables
+		self.num_training_papers = 0
+		self.num_validation_papers = 0
+		self.num_testing_papers = 0
+
+		# Mapping variables
+		self.paper_to_neuron = {}
+		self.neuron_to_paper = {}
+		self.topic_to_neuron = {}
+		self.neuron_to_topic = {}
+		
+
+	def load_paper_neurons(config, snn):
+		""" Loads the paper info and creates paper neurons in the snn
+
+			Args:
+				config: Configuration of the dataset from the yaml file
+				snn: SuperNeuroMAT SNN
+
+			Returns:
+				paper_to_neuron: Dictionary mapping paper IDs to neuron IDs
+				neuron_to_paper: Dictionary mapping neuron IDs to paper IDs
+
+		"""
+
+		if config["dataset"] == "microseer":
+			with open("data/microseer/microseer.indices"):
 
 
 
