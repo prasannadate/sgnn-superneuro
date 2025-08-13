0d04eba8a37b5ef21773753e3842f9c42c5030d1
Dr. Prasanna Date <datepa@ornl.gov>
2025-08-13 01:46:57 -0400

    created neurons for microseer and miniseer

--- gnn_citation_networks_efficient.py
+++ gnn_citation_networks_efficient.py
@@ -4,6 +4,9 @@ from superneuromat import SNN
 
 
 DATASET_CONFIG = "configs/microseer/default_microseer_config.yaml"
+# DATASET_CONFIG = "configs/miniseer/default_miniseer_config.yaml"
+# DATASET_CONFIG = "configs/citeseer/default_citeseer_config.yaml"
+# DATASET_CONFIG = "configs/cora/default_cora_config.yaml"
 
 
 class SGNN:
@@ -23,25 +26,28 @@ class SGNN:
 		self.num_training_papers = 0
 		self.num_validation_papers = 0
 		self.num_testing_papers = 0
+		self.num_papers = 0 
+		self.num_topics = 0
+		self.num_features = 0
+
+		# Training, validation, and testing paper IDs
+		self.training_paper_ids = set()
+		self.validation_paper_ids = set()
+		self.testing_paper_ids = set()
 
 		# Mapping variables: IDs
 		self.paper_to_neuron = {}
 		self.neuron_to_paper = {}
 		self.topic_to_neuron = {}
 		self.neuron_to_topic = {}
-		self.feature_to_neuron = {}
-		self.neuron_to_feature = {}
+		# self.feature_to_neuron = {}
+		# self.neuron_to_feature = {}
 
 		# Mapping variables: papers, topics, and features
 		self.paper_to_topic = {}
 		self.topic_to_paper = {}
 		self.paper_to_feature = {}
 		self.feature_to_paper = {}
-
-		# Training, validation, and testing papers
-		self.training_paper_ids = []
-		self.validation_paper_ids = []
-		self.testing_paper_ids = []
 
 		# SuperNeuroMAT SNN 
 		self.snn = SNN()
@@ -55,92 +61,161 @@ class SGNN:
 
 		# Choose file path file path based on the dataset
 		if config["dataset"] == "microseer":
-			content_file = "data/microseer/microseer.indices"
+			content_file = "data/microseer/microseer.content"
 			cites_file = "data/microseer/microseer/cites"
 
+			with open("data/microseer/microseer.indices", 'r') as file:
+				lines = file.readlines()
+
+			self.training_paper_ids.update(lines[1].strip().split(', '))
+			self.testing_paper_ids.update(lines[2].strip().split(', '))
+
+
 		elif config["dataset"] == "miniseer":
-			content_file = "data/miniseer/miniseer.indices"
+			content_file = "data/miniseer/miniseer.content"
 			cites_file = "data/miniseer/miniseer/cites"
 
+			with open("data/miniseer/miniseer.indices", 'r') as file:
+				lines = file.readlines()
+
+			self.training_paper_ids.update(lines[1].strip().split(', '))
+			self.testing_paper_ids.update(lines[2].strip().split(', '))
+
+
 		elif config["dataset"] == "citeseer":
-			content_file = "data/citeseer/citeseer.indices"
+			content_file = "data/citeseer/citeseer.content"
 			cites_file = "data/citeseer/citeseer/cites"
 
+			with open("data/citeseer/citeseer_train_nodes.txt", 'r') as file:
+				lines = file.readlines()
+
+			self.training_paper_ids.update(lines[1].strip().split(', '))
+			self.testing_paper_ids.update(lines[3].strip().split(', '))
+
+
 		elif config["dataset"] == "cora":
-			content_file = "data/cora/cora.indices"
-			cites_file = "data/cora/cora/cites"
+			content_file = "data/Cora/cora/cora.content"
+			cites_file = "data/Cora/cora/cora/cites"
 
-		elif config["dataset"] == "pubmed":
-			content_file = "data/cora/cora.indices"
-			cites_file = "data/cora/cora/cites"
+			with open("data/Cora/cora_train_indices.txt", 'r') as file:
+				lines = file.readlines()
 
+			self.training_paper_ids.update(lines[1].strip().split(', '))
+			self.testing_paper_ids.update(lines[3].strip().split(', '))
+
+		
 		# To add: pubmed and biteseer
 
+		
 		else:
 			raise ValueError("Unknown dataset in config file")
 
-
-		# Read content_file
-
-
-
-	# def load_microseer(self):
-	# 	""" Loads the microseer dataset
-
-	# 	"""
-
-	# 	# *** LOAD PAPERS ***
-	# 	with open("data/microseer/microseer.indices") as f:
-	# 		lines = f.readlines()
+		
+		# Count number of papers in training and testing sets
+		self.num_training_papers = len(self.training_paper_ids)
+		self.num_testing_papers = len(self.testing_paper_ids)
 
 
-	# 	# Extract indices of training and testing papers
-	# 	self.training_paper_ids = lines[1].strip().split()
-	# 	self.testing_paper_ids = lines[2].strip().split()
+		# Count number of features and create feature neurons
+		if config["features"] == 1:
+			with open(content_file, 'r') as file:
+				line = file.readline().strip().split()
+
+			self.num_features = len(line) - 2
+
+			for i in range(self.num_features):
+				neuron_id = self.snn.create_neuron(
+					threshold=config["feature_threshold"],
+					leak = config["feature_leak"], 
+					refractory_period = config["feature_ref"]
+				)
+
+				# self.feature_to_neuron[i] = neuron_id
+				# self.neuron_to_feature[neuron_id] = i
 
 
-	# 	# Create training neurons
-	# 	for paper_id in training_paper_ids:
-	# 		neuron_id = self.snn.create_neuron().idx
+		# Read content_file line by line and create the SNN
+		with open(content_file, 'r') as file:
+			for line in file:
 
-	# 		if paper_id not in self.paper_to_neuron:
-	# 			self.paper_to_neuron[paper_id] = neuron_id
-
-	# 		else:
-	# 			raise RuntimeError(f"Paper ID {paper_id} already exists in paper_to_neuron")
-
-	# 		if neuron_id not in self.neuron_to_paper:
-	# 			self.neuron_to_paper[neuron_id] = paper_id
-
-	# 		else:
-	# 			raise RuntimeError("Neuron ID already exists in paper_to_neuron")
+				# Gather contents of the line
+			    line_contents = line.strip().split()
 
 
-	# 	# Create testing neurons
-	# 	for paper_id in testing_paper_ids: 
-	# 		neuron_id = self.snn.create_neuron().idx
-			
-	# 		if paper_id not in self.paper_to_neuron:
-	# 			self.paper_to_neuron[paper_id] = neuron_id
-
-	# 		else:
-	# 			raise RuntimeError("Paper ID already exists in paper_to_neuron")
-
-	# 		if neuron_id not in self.neuron_to_paper:
-	# 			self.neuron_to_paper[neuron_id] = paper_id
-
-	# 		else:
-	# 			raise RuntimeError("Neuron ID already exists in paper_to_neuron")
+			    # Extract paper and topic IDs
+			    paper_id = line_contents[0]
+			    topic_id = line_contents[-1]
 
 
-	# 	# *** LOAD TOPICS AND FEATURES ***
-	# 	with open("data/microseer/microseer.content") as f:
-	# 		lines = f.readlines()
+			    # Update dictionary that maps papers to topics and vice versa
+			    if paper_id not in self.paper_to_topic:
+			    	self.paper_to_topic[paper_id] = topic_id
+
+			    else:
+			    	raise RuntimeError(f"Topic for paper_id {paper_id} already documented")
+
+			    if topic_id not in self.topic_to_paper:
+			    	self.topic_to_paper[topic_id] = [paper_id]
+
+			    else:
+			    	self.topic_to_paper[topic_id].append(paper_id)
+			    
+
+			    # Create training paper neuron
+			    if paper_id in self.training_paper_ids:
+			    	neuron_id = self.snn.create_neuron(
+			    		threshold = config["paper_threshold"],
+			    		leak = config["paper_leak"],
+			    		refractory_period = config["train_ref"]
+			    	)
+			    
+			    # Create validation paper neuron
+			    elif paper_id in self.validation_paper_ids:
+			    	neuron_id = self.snn.create_neuron(
+			    		threshold = config["paper_threshold"],
+			    		leak = config["paper_leak"],
+			    		refractory_period = config["validation_ref"]
+			    	)
+
+			    # Create test paper neuron
+			    elif paper_id in self.testing_paper_ids:
+			    	neuron_id = self.snn.create_neuron(
+			    		threshold = config["paper_threshold"],
+			    		leak = config["paper_leak"],
+			    		refractory_period = config["test_ref"]
+			    	)
+
+			    else:
+			    	raise RuntimeError(f"paper_id {paper_id} is not in training, validation, or test sets")
+			    	# neuron_id = self.snn.create_neuron()
 
 
-	# 	# 
+			    # Update the dictionaries that map papers to paper neurons and vice versa
+			    self.paper_to_neuron[paper_id] = neuron_id
+			    self.neuron_to_paper[neuron_id] = paper_id
 
-	# 	print(self.snn)
+
+			    # Create topic neuron only if it doesn't already exist
+			    if topic_id not in self.topic_to_neuron:
+			    	neuron_id = self.snn.create_neuron()
+			    	self.topic_to_neuron[topic_id] = neuron_id
+			    	self.neuron_to_topic[neuron_id] = topic_id
+
+
+
+
+			    # Create synapses from paper neurons to topic neurons
+
+
+			    # Create synapses from paper neurons to feature neurons
+
+
+			    # Update dictionary that maps papers to features and vice versa
+
+
+			    # Create synapses from paper neurons to paper neurons
+
+
 
 
 
@@ -154,4 +229,25 @@ if __name__ == "__main__":
 
 	# Load dataset
 	sgnn.load_dataset()
+
+	print(f"\nNum training papers: {sgnn.num_training_papers}")
+	print(f"\nNum testing papers: {sgnn.num_testing_papers}")
+	print(f"\nTraining paper IDs: {sgnn.training_paper_ids}")
+	print()
+
+	print(f"\nNum papers: {sgnn.num_papers}")
+	print(f"\nNum topics: {sgnn.num_topics}")
+	print(f"\nNum features: {sgnn.num_features}")
+	print()
+
+	print(f"\npaper_to_neuron: {sgnn.paper_to_neuron}")
+	print(f"\nneuron_to_paper: {sgnn.neuron_to_paper}")
+	print(f"\ntopic_to_neuron: {sgnn.topic_to_neuron}")
+	print(f"\nneuron_to_topic: {sgnn.neuron_to_topic}")
+	# print(f"\nfeature_to_neuron: {sgnn.feature_to_neuron}")
+	# print(f"\nneuron_to_feature: {sgnn.neuron_to_feature}")
+	print(f"\npaper_to_topic: {sgnn.paper_to_topic}")
+	print(f"\ntopic_to_paper: {sgnn.topic_to_paper}")
+
+	# print(sgnn.snn)
 
